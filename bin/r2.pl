7933353f00072f4b3b661ccd15cfb366ac06e30d
dongw.cs <dongw.cs@gmail.com>
2011-06-21 02:16:35 +0000

    maybe, the process of psi.pl should be integrated into the diff generation process.

--- bin/r2.pl
+++ bin/r2.pl
@@ -43,9 +43,11 @@ while (<cc>) {
     &excmd("$bi 0 $dir1 >$dir1/build/telosb/bi0.txt");
     &excmd("$bi 0 $dir2 >$dir2/build/telosb/bi0.txt");
     
-    # 3. generate out.ihex
+    # 3. generate out.ihex and also copy to out-bi0.exe for psi
     &excmd("msp430-objcopy --output-target=ihex $dir1/build/telosb/out.exe $dir1/build/telosb/out.ihex");
     &excmd("msp430-objcopy --output-target=ihex $dir2/build/telosb/out.exe $dir2/build/telosb/out.ihex");
+    &excmd("cp $dir1/build/telosb/out.exe $dir1/build/telosb/out-bi0.exe");
+    &excmd("cp $dir2/build/telosb/out.exe $dir2/build/telosb/out-bi0.exe");
     
     # 4. generate out.raw
     &excmd("perl ./hex2raw.pl $dir1/build/telosb/out.ihex $dir1/build/telosb/out.raw > $dir1/hex2raw-r2.log");
